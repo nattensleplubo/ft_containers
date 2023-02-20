423b87ec655d8702ea714e3abaaf28240e530c9d
nattensleplubo <nathan.gobert2002@gmail.com>
2023-02-20 12:49:23 +0100

    caca

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 10:18:09 by ngobert           #+#    #+#             */
-/*   Updated: 2023/02/16 13:12:09 by ngobert          ###   ########.fr       */
+/*   Updated: 2023/02/20 12:49:12 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 namespace ft {
 
-//! LEXICOGRAPHICAL COMPARE :
+//! LEXICOGRAPHICAL COMPARE : #################################################
 template<class InputIt1, class InputIt2>
 bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
                              InputIt2 first2, InputIt2 last2)
@@ -48,7 +48,7 @@ bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
 }
 
 
-//! PAIR
+//! PAIR ######################################################################
 template<class T1, class T2> struct pair {
         typedef T1    first_type;
         typedef T2    second_type;
@@ -70,15 +70,72 @@ template<class T1, class T2> struct pair {
             return (*this);
         }
     };
-template<class _T1, class _T2>
-  inline pair<typename __decay_and_strip<_T1>::__type,
-      typename __decay_and_strip<_T2>::__type>
-  make_pair(_T1&& __x, _T2&& __y)
-  {
-    return pair<typename __decay_and_strip<_T1>::__type,
-            typename __decay_and_strip<_T2>::__type>
-  (std::forward<_T1>(__x), std::forward<_T2>(__y));
-  }
+
+	template <typename T, typename U>
+	pair<T, U> make_pair(T first, U second) {
+		return (pair<T, U>(first, second));
+	}
+
+		template <typename T, typename U>
+	bool	operator==(pair<T, U> const &lhs, pair<T, U> const &rhs) {
+		return (lhs.first == rhs.first && lhs.second == rhs.second);
+	}
+
+	template <typename T, typename U>
+	bool	operator!=(pair<T, U> const &lhs, pair<T, U> const &rhs) {
+		return (!(lhs == rhs));
+	}
+
+	template <typename T, typename U>
+	bool	operator<(pair<T, U> const &lhs, pair<T, U> const &rhs) {
+		return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+	}
+
+	template <typename T, typename U>
+	bool	operator<=(pair<T, U> const &lhs, pair<T, U> const &rhs) {
+		return (!(rhs < lhs));
+	}
+
+	template <typename T, typename U>
+	bool	operator>(pair<T, U> const &lhs, pair<T, U> const &rhs) {
+		return (rhs < lhs);
+	}
+
+	template <typename T, typename U>
+	bool	operator>=(pair<T, U> const &lhs, pair<T, U> const &rhs) {
+		return (!(lhs < rhs));
+	}
+
+//! ITERATOR_TRAITS ###########################################################
+
+
+template<class Iterator>
+	struct iterator_traits {
+		typedef typename Iterator::difference_type			difference_type;
+		typedef typename Iterator::value_type				value_type;
+		typedef typename Iterator::pointer					pointer;
+		typedef typename Iterator::reference				reference;
+		typedef typename Iterator::iterator_category		iterator_category;
+	};
+
+	template<typename T>
+	struct iterator_traits<T*> {
+		typedef std::ptrdiff_t						difference_type;
+		typedef T									value_type;
+		typedef T*									pointer;
+		typedef T&									reference;
+		typedef std::random_access_iterator_tag	iterator_category;
+
+	};
+
+	template<typename T>
+	struct iterator_traits<const T*> {
+		typedef std::ptrdiff_t						difference_type;
+		typedef T									value_type;
+		typedef T const*							pointer;
+		typedef T const&							reference;
+		typedef std::random_access_iterator_tag	iterator_category;
+	};
 
 
 };
